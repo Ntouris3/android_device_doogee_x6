4bf094e78231672011253e2cb6859cb535b9c9c2
Ntouris3 <theodor.ioannou123@gmail.com>
2017-09-11 17:55:57 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 # Inherit device configuration
-$(call inherit-product, device/walton/nf2/nf2.mk)
+$(call inherit-product, device/Doogee/X6/X6.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := nf2
-PRODUCT_NAME := lineage_nf2
+PRODUCT_DEVICE := X6
+PRODUCT_NAME := cm_X6
